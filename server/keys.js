5607c6dbadd011593bf0eec6aacc972cd8c30687
kiranmurali93 <kiranmurali93@gmail.com>
2020-10-07 07:08:36 +0530

    removed keys

--- server/keys.js
+++
@@ -1,7 +0,0 @@
-// OV6dyvmk74t5etHz
-// mongodb+srv://kiran:<password>@cluster0.05bnm.mongodb.net/<dbname>?retryWrites=true&w=majority
-
-module.exports = {
-  MONGOURI: 'mongodb+srv://kiran:OV6dyvmk74t5etHz@cluster0.05bnm.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  JWT_SECRET: 'dAnCeMoNkEy',
-};
